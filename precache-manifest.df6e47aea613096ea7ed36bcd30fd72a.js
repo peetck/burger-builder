cdcfe9d6b1e2771ced12ff27c1e1e886da89de3c
peetck <peekung602@gmail.com>
2020-08-14 00:00:48 +0700

    Updates

--- precache-manifest.df6e47aea613096ea7ed36bcd30fd72a.js
+++ precache-manifest.df6e47aea613096ea7ed36bcd30fd72a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f45d268f720f08fae0e0bf0c1a91c6f",
+    "revision": "966e4ca757dfac669705b558f5aedcae",
     "url": "/burger-builder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/burger-builder/static/css/3.e48e44f0.chunk.css"
   },
   {
-    "revision": "62a9a6bd2c532d814e59",
+    "revision": "fadcb366f65e44b3196e",
     "url": "/burger-builder/static/css/4.947f1010.chunk.css"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/burger-builder/static/css/5.46104845.chunk.css"
   },
   {
-    "revision": "3a8baaab250cb390f664",
+    "revision": "7d657a26631f1ac1a73e",
     "url": "/burger-builder/static/css/main.d34a1d5c.chunk.css"
   },
   {
@@ -32,20 +32,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/burger-builder/static/js/3.f71134f9.chunk.js"
   },
   {
-    "revision": "62a9a6bd2c532d814e59",
-    "url": "/burger-builder/static/js/4.3ae14ca4.chunk.js"
+    "revision": "fadcb366f65e44b3196e",
+    "url": "/burger-builder/static/js/4.3c664b14.chunk.js"
   },
   {
     "revision": "2e50b60fdfe456de732a",
     "url": "/burger-builder/static/js/5.5e03c03f.chunk.js"
   },
   {
-    "revision": "3a8baaab250cb390f664",
-    "url": "/burger-builder/static/js/main.521d26ef.chunk.js"
+    "revision": "7d657a26631f1ac1a73e",
+    "url": "/burger-builder/static/js/main.e3297402.chunk.js"
   },
   {
-    "revision": "29cd6d7be99491f0e377",
-    "url": "/burger-builder/static/js/runtime-main.677eea59.js"
+    "revision": "ac69afeabdeaacad55ac",
+    "url": "/burger-builder/static/js/runtime-main.57dd7afe.js"
   },
   {
     "revision": "b8503d262bffbfb7c67fd6762963e7d1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.12d914d94e60dae363ae02a6e10f7d23.js"
+  "/burger-builder/precache-manifest.df6e47aea613096ea7ed36bcd30fd72a.js"
 );
 
 self.addEventListener('message', (event) => {
